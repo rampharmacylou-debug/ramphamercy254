5d7b79a8272e7c526a7bd9105f8c30cf7236dc35
rampharmacylou-debug <rampharmacylou@gmail.com>
2026-08-25 01:29:56 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,5 @@
-'use client';
-
-import { useEffect } from 'react';
-
-export default function RootPage() {
-  useEffect(() => {
-    window.location.href = '/staff/products';
-  }, []);
-
-  return null;
-}'use client';
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace('/staff/products');
-  }, [router]);
-
-  return null;
+  redirect('/staff/products');
 }

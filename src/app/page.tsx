5cca60770021512b68d166c7185510800f93de06
rampharmacylou-debug <rampharmacylou@gmail.com>
2026-08-25 01:19:38 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import { redirect } from 'next/navigation';
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  redirect('/staff/products');
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/staff/products');
+  }, [router]);
+
+  return null;
 }
